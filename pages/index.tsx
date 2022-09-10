56f9867bbf08a06afae6dadb458256a276a07b92
ygo <ka.ra.si.naporitan@gmail.com>
2022-09-10 23:54:01 +0900

    master

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 export default function Home({ blog,totalCount }: Props) {
   return (
     <>
-      <h1 className="container mx-auto px-10 pt-20 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 relative z-2">
+      <h1 className="container mx-auto text-white px-10 pt-20 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 relative z-2">
         記事一覧
       </h1>
       <div className="container mx-auto p-1 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 relative z-2">
