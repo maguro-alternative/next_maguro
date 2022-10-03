14f6d584a07f36b625464977d687fe16f2073c75
ygo <ka.ra.si.naporitan@gmail.com>
2022-10-03 13:54:23 +0900

    master

--- components/part.tsx
+++ components/part.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 type count = {
   totalCount:number
@@ -6,15 +7,37 @@ type count = {
 //ページネーションの追加
 export const Pagination = ({ totalCount }:count) => {
   const PER_PAGE = 5;
+  const router = useRouter().asPath;
+  let page_back_num:number=Number(router.slice(-1));
+  let page_forward_num:number=Number(router.slice(-1));
+  console.log(page_back_num)
+  if (page_back_num>1){
+    page_back_num--
+  }
+  if(page_forward_num<Math.ceil(totalCount / PER_PAGE)){
+    page_forward_num++
+  }
 
   const range = (start:number, end:number) =>
         [...Array(end - start + 1)].map((_, i) => start + i)
+
+        console.log(Math.ceil(totalCount / PER_PAGE))
 
   return (
     <div className="wrapper">
       <div className="mainbar">
         <div className="textback">
           <ul>
+          <li className='inline-block relative m-2'>
+                <Link  href={ `/blog/page/1`}>
+                  <a className='text-2xl'>&lt;&lt;</a>
+                </Link>
+            </li>
+            <li className='inline-block relative m-2'>
+                <Link  href={ `/blog/page/${page_back_num}`}>
+                  <a className='text-2xl'>&lt;</a>
+                </Link>
+            </li>
             {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
               <li className='inline-block relative m-2' key={index}>
                 <Link  href={ `/blog/page/${number}`}>
@@ -22,6 +45,16 @@ export const Pagination = ({ totalCount }:count) => {
                 </Link>
               </li>
             ))}
+            <li className='inline-block relative m-2'>
+                <Link  href={ `/blog/page/${page_forward_num}`}>
+                  <a className='text-2xl'>&gt;</a>
+                </Link>
+            </li>
+            <li className='inline-block relative m-2'>
+                <Link  href={ `/blog/page/${Math.ceil(totalCount / PER_PAGE)}`}>
+                  <a className='text-2xl'>&gt;&gt;</a>
+                </Link>
+            </li>
           </ul>
         </div>
       </div>
