dea3bc60d94ca83d7c1190118fd8e82127d7575b
ygo <ka.ra.si.naporitan@gmail.com>
2022-10-06 11:31:10 +0900

    master

--- components/part.tsx
+++ components/part.tsx
@@ -10,7 +10,7 @@ export const Pagination = ({ totalCount }:count) => {
   const router = useRouter().asPath;
   let page_back_num:number=Number(router.slice(-1));
   let page_forward_num:number=Number(router.slice(-1));
-  console.log(page_back_num)
+  //console.log(page_back_num)
   if (page_back_num>1){
     page_back_num--
   }
@@ -21,38 +21,38 @@ export const Pagination = ({ totalCount }:count) => {
   const range = (start:number, end:number) =>
         [...Array(end - start + 1)].map((_, i) => start + i)
 
-        console.log(Math.ceil(totalCount / PER_PAGE))
+  //console.log(Math.ceil(totalCount / PER_PAGE))
 
   return (
     <div className="wrapper">
       <div className="mainbar">
         <div className="textback">
           <ul>
-          <li className='inline-block relative m-2'>
+            <li className='inline-block relative m-2'>
                 <Link  href={ `/blog/page/1`}>
-                  <a className='text-2xl'>&lt;&lt;</a>
+                  <a className='sm:text-2xl text-sm'>&lt;&lt;</a>
                 </Link>
             </li>
             <li className='inline-block relative m-2'>
                 <Link  href={ `/blog/page/${page_back_num}`}>
-                  <a className='text-2xl'>&lt;</a>
+                  <a className='sm:text-2xl text-sm'>&lt;</a>
                 </Link>
             </li>
             {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
               <li className='inline-block relative m-2' key={index}>
                 <Link  href={ `/blog/page/${number}`}>
-                  <a className='text-2xl'>{number}</a>
+                  <a className='sm:text-2xl text-sm'>{number}</a>
                 </Link>
               </li>
             ))}
             <li className='inline-block relative m-2'>
                 <Link  href={ `/blog/page/${page_forward_num}`}>
-                  <a className='text-2xl'>&gt;</a>
+                  <a className='sm:text-2xl text-sm'>&gt;</a>
                 </Link>
             </li>
             <li className='inline-block relative m-2'>
                 <Link  href={ `/blog/page/${Math.ceil(totalCount / PER_PAGE)}`}>
-                  <a className='text-2xl'>&gt;&gt;</a>
+                  <a className='sm:text-2xl text-sm'>&gt;&gt;</a>
                 </Link>
             </li>
           </ul>
